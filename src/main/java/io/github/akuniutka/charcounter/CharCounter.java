3de201c2e5b6bc1d5a6cb02795a9c0658349d657
Andrei Kuniutka <akuniutka@gmail.com>
2023-11-10 23:04:24 +0300

    created an empty application

---
+++ src/main/java/io/github/akuniutka/charcounter/CharCounter.java
@@ -0,0 +1,11 @@
+package io.github.akuniutka.charcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharCounter {
+    public static void main(String[] args) {
+        SpringApplication.run(CharCounter.class, args);
+    }
+}
